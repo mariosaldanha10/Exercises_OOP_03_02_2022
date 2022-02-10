3901462118ac6bdaf9ed20c2db358160a60e5247
Mario Luis Saldanha Santos <mariosaldanha.ti@gmail.com>
2022-02-10 16:36:02 +0000

    changes were made

---
+++ Exercises/Question2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercises_OOP_03_02_2022
+
+{
+    
+
+    public class Question2
+    {
+        public Question2()
+        {
+
+        }
+        public void sumInt()
+        {
+            int i = 1;
+            int sum = 0;
+            while (i <= 50)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Final value: {0}", sum);
+        }
+    }
+
+}
---
+++ Exercises/Question3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_OOP_03_02_2022
+{
+    public class Question3
+    {
+        public Question3()
+        {
+
+        }
+        public void randomNumbers()
+        {
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(1, 11);
+            int usersGuess;
+            int amountOfGuesses = 0;
+            do
+            {
+                Console.WriteLine("Guess the random number");
+                usersGuess = int.Parse(Console.ReadLine());
+                if (usersGuess < randomNumber)
+                {
+                    Console.WriteLine("Your guess was too low");
+                }
+                else if (usersGuess > randomNumber)
+                {
+                    Console.WriteLine("Your guess was too high");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is correct");
+                }
+                amountOfGuesses++;
+
+            } while (usersGuess != randomNumber);
+            Console.WriteLine("You guessed {0} times", amountOfGuesses);
+        }
+    }
+}
